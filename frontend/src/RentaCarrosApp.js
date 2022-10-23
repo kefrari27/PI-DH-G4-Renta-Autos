2931833ab0b2dabd5029ff5562d8c8357081f152
Kevin FR <kefrari27@gmail.com>
2022-10-23 13:10:06 -0500

    feature FE #6 - avance implementar template bloque buscador

--- frontend/src/RentaCarrosApp.js
+++ frontend/src/RentaCarrosApp.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Browser from "./components/Browser";
 
 const RentaCarrosAPP = () => {
     return (
         <>
         <Header />
+        <Browser/>
         <Footer />
         </>
     )
