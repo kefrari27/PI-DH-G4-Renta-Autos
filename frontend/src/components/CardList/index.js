4d938a28a37f9ffc0e6e3949c6fd2830d1f02942
Maureen Parra Mondragon <maureenpm13@hotmail.com>
2022-11-07 21:52:52 -0500

    clean code

--- frontend/src/components/CardList/index.js
+++ frontend/src/components/CardList/index.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
-import { deleteFetch, getFetch, postFetch, putFetch } from '../../core/request';
+import { getFetch } from '../../core/request';
 import Card from '../Card';
+import dataMock from './data.json'
 import "./styles.css"
+
 const CardList = () => {   
 
     const CATEGORIAS_API_URL = 'http://localhost:8080/api/v1/categorias';   
     
-    const [dataCategorias, setdataCategorias] = useState([]);     
+    const [dataCategorias, setdataCategorias] = useState(dataMock);     
 
     const consultarCategorias = async()=>{     
       const data = await getFetch(CATEGORIAS_API_URL);     
